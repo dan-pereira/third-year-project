23e274838a22811fe72b1f10c0b9aa4d3bce3951
byrnj233 <jacob.byrne233@mail.dcu.ie>
2019-02-18 18:46:17 +0000

    deploy scraper on server

--- code/server_side/routes.py
+++ code/server_side/routes.py
@@ -1,10 +1,18 @@
 from app import app
 from flask import Flask, request, jsonify
-
+import bsoup
 
 @app.route('/')
 @app.route('/index')
-@app.route('/index/html')
+# @app.route('/timetables/locations/')
 def index():
-    return "Hello, Home directory!"
-    
+    # return("Hello, Home directory!")
+    room_id = 'GLA.LG27'
+    x = bsoup.get_url(room_id)
+    return (bsoup.html_to_json(x))
+
+@app.route('/timetables')
+def timetables(): 
+	return("see directories below for timetables")
+
+
