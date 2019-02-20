f31bb24759051f12491e4bc9a972ab9e8964422d
byrnj233 <jacob.byrne233@mail.dcu.ie>
2019-02-20 11:06:03 +0000

    server side edit

---
+++ code/server_side/routes.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import bsoup
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return ('Welcome to TouchTime Local Server')
+
+@app.route('/timetables/')
+def timetables(): 
+	return("see directories below for timetables")
+
+# anyinput = input()
+
+@app.route('/timetables/locations/101')
+def timetable():
+    # return("Hello, Home directory!")
+    room_id = 'GLA.L101'
+    x = bsoup.get_url(room_id)
+    return (bsoup.html_to_json(x))
+
+@app.route('/timetables/locations/any')
+def timetable_any():
+    # return("Hello, Home directory!")
+    room_id = input()
+    x = bsoup.get_url(room_id)
+    return (bsoup.html_to_json(x))
