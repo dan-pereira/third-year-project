9467ece0382142bea66a6864e9450a7448575019
byrnj233 <jacob.byrne233@mail.dcu.ie>
2019-02-26 12:00:03 +0000

    get timetable dynamically given URL

--- code/server_side/routes.py
+++ code/server_side/routes.py
@@ -12,63 +12,14 @@ def timetable():
     return("see directories below for timetables")
 
 
-@app.route('/timetables/locations/L101')
-def timetable101():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.L101'
+@app.route('/timetables/locations/<tester1>')
+def tester(tester1):
+    # x = "yurt" 
+    # return(x)
+    room_id = 'GLA.' + tester1
     x = bsoup.get_url(room_id)
     return jsonify(bsoup.html_to_json(x))
 
-
-@app.route('/timetables/locations/L114')
-def timetable114():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.L114'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
-
-@app.route('/timetables/locations/L125')
-def timetable125():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.L125'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
-
-@app.route('/timetables/locations/L128')
-def timetable128():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.L128'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
-
-@app.route('/timetables/locations/LG25')
-def timetable25():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.LG25'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
-
-@app.route('/timetables/locations/LG26')
-def timetable26():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.LG26'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
-
-@app.route('/timetables/locations/LG27')
-def timetable27():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.LG27'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
-
-@app.route('/timetables/locations/CG12')
-def timetable28():
-    # return("Hello, Home directory!")
-    room_id = 'GLA.CG12'
-    x = bsoup.get_url(room_id)
-    return jsonify(bsoup.html_to_json(x))
 
 if __name__ == '__main__':
-    app.run(host='136.206.243.212')
-
+    app.run(host='136.206.255.215', port = '5000')
