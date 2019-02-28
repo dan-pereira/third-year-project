c5bbd7afbc24ee2bd8352d1cdd111c581e273685
byrnj233 <jacob.byrne233@mail.dcu.ie>
2019-02-28 10:52:41 +0000

    tidy server side, error handling additions

--- code/server_side/routes.py
+++ code/server_side/routes.py
@@ -3,7 +3,7 @@ import bsoup
 # from app import app
 app = Flask(__name__)
 
-# CREATE BASH TO REFRESH COUNTER
+# CREATE BASH TO REFRESH COUNTER 
 
 counter_dict = {}
 
@@ -21,42 +21,23 @@ def timetable_empty(tester1):
 	x = bsoup.get_url(room_id)
 	return jsonify(bsoup.html_to_json(x))
 
-@app.route('/timetables/locations/<tester1>/lecturer')
-# if student == 'student': 
-# 	if tester1 not in counter_dict: 
-# 		counter_dict[tester1] = 1
-# 	else: 
-# 		counter_dict[tester1] += 1 
-
-# elif student == 'lecturer': 
-def lec_testertester(tester1):
+@app.route('/timetables/locations/<tester1>/lecturer') 
+def lec_tester(tester1):
 	if tester1 not in counter_dict: 
 		counter_dict[tester1] = 0
 	return str(counter_dict[tester1])
 
 # ---------------------------------------------------
-@app.route('/timetables/locations/<tester1>/student')
-# if student == 'student': 
-# 	if tester1 not in counter_dict: 
-# 		counter_dict[tester1] = 1
-# 	else: 
-# 		counter_dict[tester1] += 1 
-
-# elif student == 'lecturer': 
+@app.route('/timetables/locations/<tester1>/student') 
 def tester(tester1):
-	# x = "yurt" 
-	# return(x)
 	if tester1 not in counter_dict: 
 		counter_dict[tester1] = 1
 	else: 
 		counter_dict[tester1] += 1 
-	# elif student == 'lecturer': 
-	# 	return null
-	
 	room_id = 'GLA.' + tester1
 	x = bsoup.get_url(room_id)
 	return jsonify(bsoup.html_to_json(x))
 
 if __name__ == '__main__':
-	app.run(host='192.168.43.132', port = '5000')
+	app.run(host='136.206.255.215', port = '5000')
 
