b09b3ce6349fdb877dc3ee92d8e81754d8f471ef
byrnj233 <jacob.byrne233@mail.dcu.ie>
2019-03-04 18:01:16 +0000

    added validation for room queried

--- code/server_side/routes.py
+++ code/server_side/routes.py
@@ -1,49 +1,65 @@
-from flask import Flask, request, jsonify, render_template
+import sys
+sys.path.append('/var/www/FlaskApp/FlaskApp')
 import bsoup
-import socket
-# from app import app
-app = Flask(__name__)
+from flask import Flask, request, jsonify, render_template
+#import bsoup
 
-# CREATE BASH TO REFRESH COUNTER 
+app = Flask(__name__)
 
 counter_dict = {}
 
-@app.route('/')
-def index():
-	return ('Welcome to TouchTime Local Server')
+#for key in counter_dict: 
+#       counter_dict[key] = 0 
+
+def check_location(vari):
+        file_loc = "/var/www/FlaskApp/FlaskApp/location_list.txt"
+        file = open(file_loc, 'r')
+        if str(vari+"\n") not in file:
+                return (False)
+        else:
+                return True 
+        file.close()
+
+@app.route("/")
+def hello():
+    return "Welcome to TouchTime Server!"
 
 @app.route('/timetables/')
+@app.route('/timetables/locations/')
 def timetable(): 
-	return("see directories below for timetables")
+        return("see directories below for timetables")
 
 @app.route('/timetables/locations/<tester1>/')
 def timetable_empty(tester1): 
-	room_id = 'GLA.' + tester1
-	x = bsoup.get_url(room_id)
-	return jsonify(bsoup.html_to_json(x))
+        room_id = 'GLA.' + tester1
+        if check_location(room_id) == True:
+                x = bsoup.get_url(room_id)
+                return jsonify(bsoup.html_to_json(x))
+        else:
+                return ("0") 
 
 @app.route('/timetables/locations/<tester1>/lecturer') 
 def lec_tester(tester1):
-	if tester1 not in counter_dict: 
-		counter_dict[tester1] = 0
-	return str(counter_dict[tester1])
+        if tester1 not in counter_dict: 
+                counter_dict[tester1] = 0
 
-# ---------------------------------------------------
 @app.route('/timetables/locations/<tester1>/student') 
 def tester(tester1):
-	if tester1 not in counter_dict: 
-		counter_dict[tester1] = 1
-	else: 
-		counter_dict[tester1] += 1 
-	room_id = 'GLA.' + tester1
-	x = bsoup.get_url(room_id)
-	return jsonify(bsoup.html_to_json(x))
+        if tester1 not in counter_dict: 
+                counter_dict[tester1] = 1
+        else: 
+                counter_dict[tester1] += 1 
+        room_id = 'GLA.' + tester1
+        if check_location(room_id) == True:
+                x = bsoup.get_url(room_id)
+                return jsonify(bsoup.html_to_json(x))
+        else:
+                return ("0") 
+        x = bsoup.get_url(room_id)
+        return jsonify(bsoup.html_to_json(x))
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return str(s.getsockname()[0])
 
-if __name__ == '__main__':
-	app.run(host=get_ip_address(), port = '5000')
+if __name__ == "__main__":
+    app.run()
+
 
