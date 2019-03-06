0a15e453591794b4df52c94fbe827d093f948a88
byrnj233 <jacob.byrne233@mail.dcu.ie>
2019-03-06 14:25:04 +0000

    troubleshoot counter

--- code/server_side/routes.py
+++ code/server_side/routes.py
@@ -1,46 +1,26 @@
 import sys
+
 sys.path.append('/var/www/FlaskApp/FlaskApp')
 import bsoup
-import authentication
 from flask import Flask, request, jsonify, render_template
 #import bsoup
+import authentication
 
 app = Flask(__name__)
 
 counter_dict = {}
 
 #for key in counter_dict: 
-#       counter_dict[key] = 0 
+#   counter_dict[key] = 0 
 
 def check_location(vari):
-        file_loc = "/var/www/FlaskApp/FlaskApp/location_list.txt"
-        file = open(file_loc, 'r')
-        if str(vari+"\n") not in file:
-                return (False)
-        else:
-                return True 
-        file.close()
-
-def attendance(room, user):
-    file_location = "/var/www/FlaskApp/FlaskApp/attendance_log.txt.txt"
-    file = open(file_location, 'r')
-    att_dic = {}
-    for line in file:
-        (key, val) = line.split()
-        att_dic[key] = val
+    file_loc = "/var/www/FlaskApp/FlaskApp/location_list.txt"
+    file = open(file_loc, 'r')
+    if str(vari+"\n") not in file:
+            return (False)
+    else:
+        return True 
     file.close()
-
-    if room in att_dic and user == "lecturer":
-        return att_dic[room]
-
-    if room in att_dic and user == "student":
-        att_dic[room] += 1
-    else
-        file_location = "/var/www/FlaskApp/FlaskApp/attendance_log.txt.txt"
-        file = open(file_location, 'w')
-        string = room + " " + "0"
-        file.write()
-        file.close()
 
 @app.route("/")
 def hello():
@@ -49,36 +29,49 @@ def hello():
 @app.route('/timetables/')
 @app.route('/timetables/locations/')
 def timetable(): 
-        return("see directories below for timetables")
+    return("see directories below for timetables")
 
 @app.route('/timetables/locations/<tester1>/')
 def timetable_empty(tester1): 
-        room_id = 'GLA.' + tester1
-        if check_location(room_id) == True:
-                x = bsoup.get_url(room_id)
-                return jsonify(bsoup.html_to_json(x))
-        else:
-                return ("00") 
-
-@app.route('/timetables/locations/<tester1>/lecturer') ######
-def lec_tester(tester1):
-        room_id = 'GLA.' + tester1
-        if check_location(room_id) == True:
-                return attendance(tester1,"lecturer")
-        else: 
-                return ('00')
-
-@app.route('/timetables/locations/<tester1>/student') ########
-def tester(tester1):
-        room_id = 'GLA.' + tester1
-        if check_location(room_id) == True:
-                attendance(tester1, "student")
-                x = bsoup.get_url(room_id)
-                return jsonify(bsoup.html_to_json(x))
-        else:
-                return ("0") 
+    room_id = 'GLA.' + tester1
+    if check_location(room_id) == True:
         x = bsoup.get_url(room_id)
         return jsonify(bsoup.html_to_json(x))
+    else:
+        return ("00") 
+
+@app.route('/timetables/locations/<tester1>/lecturer') 
+def lec_tester(tester1):
+    room_id = 'GLA.' + tester1
+    if check_location(room_id) == True:
+        if tester1 not in counter_dict:
+            counter_dict[tester1] = 0
+        return str(counter_dict[tester1])
+    else: 
+        return ('00')
+
+@app.route('/timetables/locations/<tester1>/student') 
+def tester(tester1):
+    if tester1 not in counter_dict: 
+        counter_dict[tester1] = 1
+    else: 
+        counter_dict[tester1] += 1 
+    room_id = 'GLA.' + tester1
+    if check_location(room_id) == True:
+        x = bsoup.get_url(room_id)
+        return jsonify(bsoup.html_to_json(x))
+    else:
+        return ("0") 
+    x = bsoup.get_url(room_id)
+    return jsonify(bsoup.html_to_json(x))
+
+
+#@app.route("/timetables/locations/LG26/")
+#def timetable():
+#    room_id = "GLA.LG26"
+#    x = bsoup.get_url(room_id)
+#    return jsonify(bsoup.html_to_json(x))
+
 
 @app.route('/authentication/<usr>/<pas>')
 def authentication_check(usr, pas):
@@ -90,5 +83,3 @@ def authentication_check(usr, pas):
 
 if __name__ == "__main__":
     app.run()
-
-
